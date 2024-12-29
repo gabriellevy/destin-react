7303f56a90954a103e3c78df242036d2203fcc4a
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2024-12-29 15:41:51 +0100

    plus de crimes

--- src/donnees/histoire/carrieres/evts_crime.ts
+++ src/donnees/histoire/carrieres/evts_crime.ts
@@ -1,5 +1,5 @@
 import {Perso} from "../../../types/Perso.ts";
-import {metiersObjs} from "../../../types/metiers/metiers.ts";
+import {metiersEnum, metiersObjs} from "../../../types/metiers/metiers.ts";
 import {GroupeEvts} from "../../../types/Evt.ts";
 import {compareStatut, MetalStatut} from "../../../types/Statut.ts";
 
@@ -19,6 +19,20 @@ export const evts_crime: GroupeEvts = {
                 "Aujourd'hui vous les avez aidés à extorquer de largent à un commerçant. "
         },
             conditions: (perso: Perso): boolean => !perso.carriere && !compareStatut(perso.statut, {metalStatut: MetalStatut.argent, rang: 4}),
+            proba: 5,
+        },
+        {
+            id: "evts_crime2",
+                description: (): string =>  "Vous prenez un mauvais coup de couteau lors d'une des nombreuses bagarres de votre carrière de malandrin. " +
+                "Vous aurez une vilaine cicatrice près de l'oeil jusqu'à la fin de vos jours en souvenir. ",
+            conditions: (perso: Perso): boolean => perso.carriere?.metier.nom === metiersEnum.ranconneur,
+            proba: 5,
+        },
+        {
+            id: "evts_crime3",
+                description: (): string => "Vous êtes maintenant un membre de la bande à part entière. " +
+                "En signe d'appartenance et de fraternité un couteau et une larme vous sont tatoués bien visibles sur le visage. ",
+            conditions: (perso: Perso): boolean => perso.carriere?.metier.nom === metiersEnum.ranconneur && perso.carriere?.duree >= 40,
             proba: 5,
         },
     ],
