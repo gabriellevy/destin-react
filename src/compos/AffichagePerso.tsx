466008b0efe57d55377acaf8f96f865b2efca2fd
gabriellevy <mdeharbe@hotmail.com>
2025-01-25 22:48:16 +0100

    affichageCarriere

--- src/compos/AffichagePerso.tsx
+++ src/compos/AffichagePerso.tsx
@@ -1,8 +1,9 @@
 import {Box, Button, List, ListItem, ListItemText, Stack, Typography} from '@mui/material';
 import {age} from "../types/Date.ts";
-import {getCaracNbDeTestsFaits, getCaracValue, TypeCarac} from "../types/caracs/Caracs.ts";
+import {getCaracValue, TypeCarac} from "../types/caracs/Caracs.ts";
 import {useContext} from "react";
 import {PersoContexte, PersoContexteType} from "../contexte/ContexteTypes.ts";
+import {Carriere, metiersEnum} from "../types/metiers/metiers.ts";
 
 interface AffichagePersoProps {
     exporter: () => void;
@@ -10,25 +11,36 @@ interface AffichagePersoProps {
 
 export default function AffichagePerso({ exporter }: Readonly<AffichagePersoProps>) {
     const { perso } = useContext(PersoContexte) as PersoContexteType;
+
+    const affichageCarriere = (carriere: Carriere) => {
+        let intituleMetier: string = carriere.metier.intitule(perso, carriere);
+        if (carriere.guilde) {
+            intituleMetier += ' ('+carriere.guilde+')';
+        }
+
+        return (carriere.metier && carriere.actif &&
+            <ListItem>
+                <ListItemText primary={intituleMetier}
+                              secondary={`(${carriere.duree} jours)`}/>
+            </ListItem>
+        );
+    }
+
     return (
-        <Box component="nav" aria-label="My site" sx={{ flexGrow: 1 }}>
+        <Box component="nav" sx={{ flexGrow: 1 }}>
             <List dense>
                 <ListItem>
                     <Typography variant="h5" gutterBottom>
                         <ListItemText primary={perso.nom} secondary={`${age(perso)} ans`}/>
                     </Typography>
                 </ListItem>
-                <ListItem>
                 {
-                    Array.from(perso.carrieres).map(([key, value]) => {
-                        return ((key && value.metier && value.actif &&
-                            <ListItemText primary={value.metier.intitule(perso, value)}
-                                          secondary={`(${value.duree} jours)`}/>)
-                            && value.guilde && <ListItemText primary={value.guilde} />
-                        )
+                    Array.from(perso.carrieres).map(([key, value]: [metiersEnum, Carriere]) => {
+                        return (key && value &&
+                            affichageCarriere(value)
+                        );
                     })
                 }
-                </ListItem>
                 <ListItem>
                     <ListItemText primary="Talents" secondary={perso.talents.join(', ')}/>
                 </ListItem>
@@ -44,18 +56,6 @@ export default function AffichagePerso({ exporter }: Readonly<AffichagePersoProp
                 <ListItemText primary="Int" secondary={getCaracValue(perso, TypeCarac.int)}/>
                 <ListItemText primary="FM" secondary={getCaracValue(perso, TypeCarac.fm)}/>
                 <ListItemText primary="Soc" secondary={getCaracValue(perso, TypeCarac.soc)}/>
-            </Stack>
-            <Stack direction="row" spacing={0}>
-                <ListItemText primary="CC" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.cc)}/>
-                <ListItemText primary="CT" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.ct)}/>
-                <ListItemText primary="F" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.f)}/>
-                <ListItemText primary="E" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.e)}/>
-                <ListItemText primary="I" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.i)}/>
-                <ListItemText primary="Ag" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.ag)}/>
-                <ListItemText primary="Dex" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.dex)}/>
-                <ListItemText primary="Int" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.int)}/>
-                <ListItemText primary="FM" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.fm)}/>
-                <ListItemText primary="Soc" secondary={getCaracNbDeTestsFaits(perso, TypeCarac.soc)}/>
             </Stack>
             <Box mt={2}>
                 <Button variant="contained" color="primary" onClick={exporter} size="small">
