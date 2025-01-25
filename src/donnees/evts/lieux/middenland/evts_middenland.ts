a5792fbecde7c45f5690b2198456fc604766a4ff
gabriellevy <mdeharbe@hotmail.com>
2025-01-25 17:05:06 +0100

    evts_middenland2

--- src/donnees/evts/lieux/middenland/evts_middenland.ts
+++ src/donnees/evts/lieux/middenland/evts_middenland.ts
@@ -1,7 +1,10 @@
 import {GroupeEvts} from "../../../../types/Evt.ts";
 import {Perso} from "../../../../types/Perso.ts";
 import {Province} from "../../../geographie/provinces.ts";
-import {editSurLesMutants, finCampagneInterieur} from "../../../dates/ennemi_interieur.ts";
+import {
+    editSurLesMutants,
+    finCampagneInterieur,
+} from "../../../dates/ennemi_interieur.ts";
 
 export const evts_middenland: GroupeEvts = {
     evts: [
@@ -14,6 +17,14 @@ export const evts_middenland: GroupeEvts = {
                 && perso.date >= editSurLesMutants
                 && perso.date <= finCampagneInterieur,
         },
+        {
+            id: "evts_middenland2",
+            description: (): string => "<i>Les rats des égouts de Middenheim ne sont pas comme les rats que l'on trouve dans le sud. "
+            + "Ils mesurent plus d'un mètre quatre-vingt et certains d'entre eux se tiennent même debout, portent des vêtements et parlent en couinant et en gloussant. "
+                + "Les ratiers de Middenheim méritent bien leur salaire c'est sûr !.</i> ",
+            conditions: (perso: Perso): boolean => perso.lieu.province === Province.middenland,
+            proba: 1,
+        },
     ],
-    probaParDefaut: 2,
+    probaParDefaut: 4,
 }
