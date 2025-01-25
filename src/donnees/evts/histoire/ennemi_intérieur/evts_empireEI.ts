256d2eaf584275b9669ca70a98f3efd6d0c4985c
gabriellevy <mdeharbe@hotmail.com>
2025-01-25 18:28:51 +0100

    rumeurs8 PdT 14

--- src/donnees/evts/histoire/ennemi_intérieur/evts_empireEI.ts
+++ src/donnees/evts/histoire/ennemi_intérieur/evts_empireEI.ts
@@ -4,7 +4,7 @@ import {Pays} from "../../../geographie/pays.ts";
 import {
     assassinatDeVonTasseninck,
     finCampagneInterieur,
-    grafBorisArriveAAltdorf
+    grafBorisArriveAAltdorf, unAnAvantDebutCampagne
 } from "../../../dates/ennemi_interieur.ts";
 
 export const evts_empireEI: GroupeEvts = {
@@ -36,10 +36,21 @@ export const evts_empireEI: GroupeEvts = {
         },
         {
             id: "evts_empireEI4",
-            description: (): string => "Tous ces nouveaux impôts ne peuvent signifier qu'une chose : le Graf remplit ses coffres pour engager des troupes et des mercenaires "
+            description: (): string => "Tous ces nouveaux impôts ne peuvent signifier qu'une chose : le Graf remplit ses coffres pour engager des troupes et des mercenaires. "
             + "Il se prépare peut-être à intervenir dans le conflit croissant entre l'Ostland et le Talabecland, ou il a peut-être l'intention de rompre avec l'empereur et de fortifier les rives nord du Reik et du Talabec pour se défendre contre les agressions du sud",
             conditions: (perso: Perso): boolean => perso.lieu.pays === Pays.empire
                 && perso.date >= assassinatDeVonTasseninck
+                && perso.date <= grafBorisArriveAAltdorf,
+            proba: 1,
+        },
+        {
+            id: "evts_empireEI5",
+            description: (): string => "Une hérésie se répand selon laquelle Sigmar n'a jamais été un dieu, juste un héros mortel couronné empereur par le grand prêtre d'Ulric. "
+                + "On raconte qu'Ulric serait le seul véritable dieu de l'empire et que les Sigmarites seraient des hérétiques. "
+                + "Les tensions entre le nord et le sud vont et viennent, mais tout le malaise semble se concentrer autour de ces différents religieux. "
+                + "Certains fanatiques ulricains demandent même à ce que le nord rompent avec le reste de l'Empire et devienne une nation à part entière. ",
+            conditions: (perso: Perso): boolean => perso.lieu.pays === Pays.empire
+                && perso.date >= unAnAvantDebutCampagne
                 && perso.date <= grafBorisArriveAAltdorf,
             proba: 1,
         },
