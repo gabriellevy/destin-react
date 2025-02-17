15b03ff3f78bf1c6cfafcc520973de9b15132ac8
gabriellevy <mdeharbe@hotmail.com>
2025-02-17 23:08:36 +0100

    suite des fous de gotheim

--- src/donnees/evts/lieux/reikland/ubersreik/evts_gotheim.ts
+++ src/donnees/evts/lieux/reikland/ubersreik/evts_gotheim.ts
@@ -15,6 +15,8 @@ export const evts_gotheim: GroupeEvts = {
             id: "evts_gotheim1",
             description: (perso: Perso): string => {
                 const carriere: Carriere|undefined = getCarriereActive(perso);
+                let forgeBrule: boolean = false;
+                const villageInonde: boolean = false;
                 let texte = "Vous êtes réveillé en sursaut en pleine nuit. "
                 + "Un grand fracas a lieu très près, des hommes hurlent. Vous avez l'impression d'apercevoir une silhouette massive par la fenêtre mais il fait trop sombre et votre tête vous torture trop pour être sûr de vous."
                 + "Un grand beuglement se fait entendre. Jamais vous n'avez rien entendu de semblable. "
@@ -55,8 +57,58 @@ export const evts_gotheim: GroupeEvts = {
                             perso.mort = true;
                         }
                     } break;
+                    case metiersEnum.forgeron:
+                    case metiersEnum.apprenti_Forgeron: {
+                        texte += "Vous avez bien assez vu la créature : il s'agit d'un énorme crapaud. "
+                        + "Il vient très probablement des marais du nord. Vous l'avez clairement vu attaquer les ges avec son énorme langue collante. "
+                        + "Votre idée géniale est d'ajouter des pointes sur une armure de plates sur laquelle vous travailliez et ainsi quand la créature tentera de vous attraper avec sa langue elle se blessera ! "
+                        + "Emli Bauer le brasseur, Kai le maçon et Bruno le bolanger vous ont rejoint et acceptent vite votre autorité dans ce travail héroïque ! "
+                        + "Vous leur ordonnez d'apporter des bûches encor et encore pour attiser le feu et perdez toute mesure."
+                        + "Il faut dire que cet idiot d'Emil affirme que le monstre est allergique à son schnaps. Ridicule !";
+                        forgeBrule = true;
 
+                    } break;
+                    case metiersEnum.brasseur:
+                    case metiersEnum.apprenti_brasseur: {
+                        texte += "Vous avez à peine vu la créature mais aucun doute : elle a détruit toutes les maisons autour de la vôtre mais vous a épargné. " +
+                            + "C'est parce qu'elle est manifestement allergique au schnaps que vous produisez ! "
+                        + "Vous voyez qu'au dehors la forge a elle aussi été épargnée et fonctionne, vous vous y rendez donc vite pour annoncer votre découverte et votre plan pour vaincre le monstre. "
+                        + "Là bas en plus d'Hugo le forgeron vous êtes heureux de trouver Kai le maçon et Bruno le boulanger. "
+                            + "Hugo construit une armure à pointes pour blesser le monstre s'il l'attaque. Il faut l'enduire de scnaps pour empoisonner le monstre ! "
+                        + "Malheureusement cette brute d'Hugo n'apprécie pas votre idée et la repousse. "
+                        + "Il prétend que la créature est un crapaud. Ridicule : vous avez distinctement vu ses cornes."
+                        + "Vous décidez de bouder pour protester. ";
+                        forgeBrule = true;
+
+                    } break;
+                    case metiersEnum.macon: {
+                        texte += "Vous avez à peine vu la créature mais une chose est sûre : vous l'avez vue s'envoler dans les airs dans un battement d'ailes. "
+                        + "Vous voyez qu'au dehors la forge a elle aussi été épargnée et fonctionne, vous vous y rendez vite pour voir si vous pouvez apprendre ce qui s'est passé et aider. "
+                        + "Là bas en plus d'Hugo le forgeron vous êtes heureux de trouver Emil le brasseur et Bruno le boulanger. "
+                        + "Hugo construit une armure à pointes pour blesser le monstre s'il l'attaque avec sa 'longue langue de crapaud'. Admettons... "
+                        + "Vous voulez aider en tout cas et suivez els ordres de Hugo pour ajouter du bois au feu de la forge, encore et encore. ";
+                        forgeBrule = true;
+
+                    } break;
+                    case metiersEnum.apprenti_boulanger:
+                    case metiersEnum.boulanger: {
+                        texte += "Vous avez à peine vu la créature mais une chose est sûre : "
+                        + "elle a été blessée par la prêtresse de Sigmar et vous avez vu de vos yeux sa blessure se refermer. "
+                            + "Il s'agit donc clairement d'un troll régénérant !"
+                        + "Vous voyez qu'au dehors la forge a elle aussi été épargnée et fonctionne, vous vous y rendez vite pour voir si vous pouvez apprendre ce qui s'est passé et aider. "
+                        + "Là bas en plus d'Hugo le forgeron vous êtes heureux de trouver Emil le brasseur et Kai la maçon. "
+                        + "Hugo construit une armure à pointes pour blesser le monstre s'il l'attaque avec sa 'longue langue de crapaud'. "
+                        + "Vpis avez une bien meilleure idée : il faut chauffer l'enclume à l'extrême et la faire tomber sur le troll qui sera sensible au feu comme tous les trolls. "
+                            + " Malheureusement Hugo ne trouve aps votre idée très raisonnable logistiquement parlant et refuse. C'est sa forge... "
+                        + "Vous voulez aider tout de même et suivez les ordres de Hugo pour ajouter du bois au feu de la forge, encore et encore. ";
+                        forgeBrule = true;
+
+                    } break;
                 }
+                if (forgeBrule) {
+                    texte += "Emportés dans leur élan et leur folie, les villageois alimentent la forge à l'excès et elle prend feu, détruisant tout le bâtiment. ";
+                }
+
                 return texte;
             },
             conditions: (perso: Perso): boolean => perso.lieu.ville === Ville.gotheim,
